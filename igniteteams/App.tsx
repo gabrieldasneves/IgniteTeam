6cb51f0de0c0458369e6c6052fc725b19edf8373
gabrielalvesbrazil <125869083+gabrielalvesbrazil@users.noreply.github.com>
2023-07-16 12:23:59 +0900

    google fonts and loader

--- igniteteams/App.tsx
+++ igniteteams/App.tsx
@@ -1,10 +1,18 @@
 import { Groups } from "@screens/Groups";
 import { ThemeProvider } from "styled-components";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 import theme from "./src/theme";
+import { ActivityIndicator } from "react-native";
 export default function App() {
+  const [fontsloaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {fontsloaded ? <Groups /> : <ActivityIndicator />}
     </ThemeProvider>
   );
 }
