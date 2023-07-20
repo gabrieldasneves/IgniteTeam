348da3af0bc375a84d74ed55a5bec4cf19487360
gabrielalvesbrazil <125869083+gabrielalvesbrazil@users.noreply.github.com>
2023-07-20 16:39:23 +0900

    Icon library to make backbutton

--- igniteteams/src/components/header/index.tsx
+++ igniteteams/src/components/header/index.tsx
@@ -1,8 +1,16 @@
-import { Container, Logo } from "./styles";
+import { Container, Logo, BackButton, BackIcon } from "./styles";
 import logoimg from "@assets/logo.png";
-export function Header() {
+type props = {
+  showBackButton?: boolean;
+};
+export function Header({ showBackButton = false }: props) {
   return (
     <Container>
+      {showBackButton && (
+        <BackButton>
+          <BackIcon />
+        </BackButton>
+      )}
       <Logo source={logoimg} />
     </Container>
   );
