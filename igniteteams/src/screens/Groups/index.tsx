a0d30001444dd4ed92da0d4e0aa85ef55e7a32f7
gabrielalvesbrazil <125869083+gabrielalvesbrazil@users.noreply.github.com>
2023-07-21 12:41:06 +0900

    creating GroupCard Component

--- igniteteams/src/screens/Groups/index.tsx
+++ igniteteams/src/screens/Groups/index.tsx
@@ -1,11 +1,13 @@
 import { Header } from "@components/header";
 import { Container } from "./styles";
 import { Highlight } from "@components/highlight";
+import { GroupCard } from "@components/groupCard";
 export function Groups() {
   return (
     <Container>
       <Header />
       <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
+      <GroupCard title="galera do mal" />
     </Container>
   );
 }
