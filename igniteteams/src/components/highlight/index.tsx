a5dfef75fbfc312e969ec8ed0c139124eb94b6f2
gabrielalvesbrazil <125869083+gabrielalvesbrazil@users.noreply.github.com>
2023-07-21 12:19:45 +0900

    Criando componente highlight (header Title))

---
+++ igniteteams/src/components/highlight/index.tsx
@@ -0,0 +1,14 @@
+import { Container, Title, Subtitle } from "./style";
+
+type props = {
+  title: string;
+  subtitle: string;
+};
+export function Highlight({ title, subtitle }: props) {
+  return (
+    <Container>
+      <Title>{title}</Title>
+      <Subtitle>{subtitle}</Subtitle>
+    </Container>
+  );
+}
--- igniteteams/src/screens/Groups/index.tsx
+++ igniteteams/src/screens/Groups/index.tsx
@@ -1,9 +1,11 @@
 import { Header } from "@components/header";
 import { Container } from "./styles";
+import { Highlight } from "@components/highlight";
 export function Groups() {
   return (
     <Container>
-      <Header showBackButton />
+      <Header />
+      <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
     </Container>
   );
 }
