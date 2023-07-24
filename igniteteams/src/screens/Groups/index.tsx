2d41d3ab30a41c04b82ad1b7b7eceb6b664773f0
gabrielalvesbrazil <125869083+gabrielalvesbrazil@users.noreply.github.com>
2023-07-24 12:28:20 +0900

    listing groups

--- igniteteams/src/screens/Groups/index.tsx
+++ igniteteams/src/screens/Groups/index.tsx
@@ -2,12 +2,25 @@ import { Header } from "@components/header";
 import { Container } from "./styles";
 import { Highlight } from "@components/highlight";
 import { GroupCard } from "@components/groupCard";
+import { useState } from "react";
+import { FlatList } from "react-native";
+
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([
+    "Galera do mal",
+    "turminha do balao",
+    "brabos",
+  ]);
+
   return (
     <Container>
       <Header />
       <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
-      <GroupCard title="galera do mal" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+      />
     </Container>
   );
 }
