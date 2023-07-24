ef12d4ec52565832c63ad1728f4efc222cc8992f
gabrielalvesbrazil <125869083+gabrielalvesbrazil@users.noreply.github.com>
2023-07-24 12:58:28 +0900

    empty list component

--- igniteteams/src/screens/Groups/index.tsx
+++ igniteteams/src/screens/Groups/index.tsx
@@ -4,22 +4,24 @@ import { Highlight } from "@components/highlight";
 import { GroupCard } from "@components/groupCard";
 import { useState } from "react";
 import { FlatList } from "react-native";
+import { ListEmpty } from "@components/listEmpty";
 
 export function Groups() {
-  const [groups, setGroups] = useState<string[]>([
-    "Galera do mal",
-    "turminha do balao",
-    "brabos",
-  ]);
+  const [groups, setGroups] = useState<string[]>([]);
 
   return (
     <Container>
       <Header />
       <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
+
       <FlatList
         data={groups}
         keyExtractor={(item) => item}
         renderItem={({ item }) => <GroupCard title={item} />}
+        ListEmptyComponent={
+          <ListEmpty message="Cadastre seu primeiro grupo!" />
+        }
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
       />
     </Container>
   );
