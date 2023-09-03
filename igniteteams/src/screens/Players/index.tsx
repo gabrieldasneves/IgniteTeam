c9b0bf3898abd3ab1d3d837e7d4f8e2a5eb6fff0
gabrielalvesbrazil <125869083+gabrielalvesbrazil@users.noreply.github.com>
2023-09-03 17:00:29 +0900

    FlatList for players

--- igniteteams/src/screens/Players/index.tsx
+++ igniteteams/src/screens/Players/index.tsx
@@ -4,8 +4,13 @@ import { Highlight } from "@components/highlight";
 import { ButtonIcon } from "@components/ButtonIcon";
 import { Input } from "@components/Input";
 import { Filter } from "@components/Filter";
+import { FlatList } from "react-native";
+import { useState } from "react";
+import { HeaderList, NumbersOfPlayers } from "@components/Filter/styles";
 
 export function Players() {
+  const [team, setTeam] = useState("TEAMA");
+  const [players, setPlayers] = useState([]);
   return (
     <Container>
       <Header showBackButton />
@@ -13,11 +18,27 @@ export function Players() {
         title="Group name"
         subtitle="Add your friends and create teams"
       />
-      <Filter title="TEAM A" isActive />
+
       <Form>
         <Input placeholder="Member" autoCorrect={false} />
         <ButtonIcon icon="add" />
       </Form>
+
+      <HeaderList>
+        <FlatList
+          data={["TEAMA", "TEAMB"]}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <Filter
+              onPress={() => setTeam(item)}
+              isActive={item === team}
+              title={item}
+            />
+          )}
+          horizontal
+        />
+        <NumbersOfPlayers>{players.length}</NumbersOfPlayers>
+      </HeaderList>
     </Container>
   );
 }
