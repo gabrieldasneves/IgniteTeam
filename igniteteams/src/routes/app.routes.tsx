42061ec959ff769fd9a3b147946c4b4c090bd56b
gabrielalvesbrazil <125869083+gabrielalvesbrazil@users.noreply.github.com>
2023-09-04 20:48:12 +0900

    stack navigation implementation

---
+++ igniteteams/src/routes/app.routes.tsx
@@ -0,0 +1,14 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Groups } from "@screens/Groups";
+import { Players } from "@screens/Players";
+import { NewGroup } from "@screens/NewGroup";
+const { Navigator, Screen } = createNativeStackNavigator();
+export function AppRoutes() {
+  return (
+    <Navigator>
+      <Screen name="groups" component={Groups} />
+      <Screen name="Players" component={Players} />
+      <Screen name="NewGroup" component={NewGroup} />
+    </Navigator>
+  );
+}
