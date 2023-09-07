d5b25ee41bc69d31f84c3dc66ef27b2930fde75e
gabrielalvesbrazil <125869083+gabrielalvesbrazil@users.noreply.github.com>
2023-09-07 19:37:03 +0900

    navigate to new

--- igniteteams/src/routes/app.routes.tsx
+++ igniteteams/src/routes/app.routes.tsx
@@ -2,13 +2,15 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Groups } from "@screens/Groups";
 import { Players } from "@screens/Players";
 import { NewGroup } from "@screens/NewGroup";
+
 const { Navigator, Screen } = createNativeStackNavigator();
+
 export function AppRoutes() {
   return (
     <Navigator screenOptions={{ headerShown: false }}>
       <Screen name="groups" component={Groups} />
       <Screen name="Players" component={Players} />
-      <Screen name="NewGroup" component={NewGroup} />
+      <Screen name="new" component={NewGroup} />
     </Navigator>
   );
 }
